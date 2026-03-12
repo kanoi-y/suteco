d9b320ddbf46594177cce95193dee828a46cd00f
kanoi-y <furafuratonbo@gmail.com>
2026-03-12 21:29:03 +0900

    🐛 CI上の型エラーを修正

--- src/lib/datasets.ts
+++ src/lib/datasets.ts
@@ -3,6 +3,6 @@ import type { MunicipalityDataset } from '@/schema/municipality-dataset-schema';
 // datasets ディレクトリ内のすべての .json ファイルを自動でrequireする
 const context = require.context('../../datasets', false, /\.json$/);
 
-export const defaultDatasets: MunicipalityDataset[] = context.keys().map((key) => {
+export const defaultDatasets: MunicipalityDataset[] = context.keys().map((key: string) => {
   return context(key) as MunicipalityDataset;
 });
